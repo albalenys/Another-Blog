570bbad25e2c811198cadfc1cb8707975a03ef3b
Abby <email": null>
2015-09-04 16:33:19 -0400

    add ability to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.new(post_params.merge(user_id: session[:user_id]))
+    if post.save
+      redirect_to action:"index", controller:"welcome"
+    else
+      error
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
