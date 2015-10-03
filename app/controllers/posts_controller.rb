84eff6c1a148668f803880622e2886aaf61803c5
Albalenys <albalenysd@gmail.com>
2015-10-02 20:10:07 -0400

    paginate comments in post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find(params[:id])
+    @comments = @post.comments.paginate(:page => params[:page], :per_page => 10)
   end
 
   private
