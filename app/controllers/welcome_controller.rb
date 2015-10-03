034687dd59568c3eb28eff4488fc87d80730394f
Albalenys <albalenysd@gmail.com>
2015-10-02 20:06:03 -0400

    change root welcome#index to post#index

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  before_filter :authorize, except: [:index]
-
-  def index
-    @posts = Post.all.paginate(:page => params[:page], :per_page => 20)
-  end
-end
