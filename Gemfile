0dad3cfa9c7fef275bbc54f302d2fac6ab3121a5
Albalenys <albalenysd@gmail.com>
2015-11-24 17:17:39 -0500

    add fancybox iframe to new post form

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
+gem 'fancybox2-rails', '~> 0.2.8'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'gravatarify', '~> 3.0.0'
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+  $("a.fancybox").fancybox();
+
+  $(".fancybox-form").submit(function(event) {
+    parent.$.fancybox.close();
+  });
+
   $(".slider-content").hide();
   $(".slider").hoverIntent(function(){
     $(".slider-content").slideToggle('slow');
