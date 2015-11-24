f68f81e132a1f12660f11b7ac1edd05a4964cbac
Albalenys <albalenysd@gmail.com>
2015-11-24 12:12:24 -0500

    capitalize titles and usernames before saving to db

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  before_save :capitalize_title
+
+  def capitalize_title
+    self.title.capitalize!
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  before_save :capitalize_name
+
+  def capitalize_name
+    self.username.capitalize!
+  end
 end
